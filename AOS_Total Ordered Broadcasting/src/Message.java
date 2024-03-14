2d979d827c8029e6aaba60dc362bea8ed2650bea
deeppadmani <125155392+deeppadmani@users.noreply.github.com>
2024-03-14 01:31:17 -0500

    new branch

---
+++ AOS_Total Ordered Broadcasting/src/Message.java
@@ -0,0 +1,53 @@
+
+import java.io.*;
+
+// Class representing a message that can be sent over the network
+public class Message implements Serializable {
+	int NodeId;         // ID of the node that created the message
+    int MsgCount ;      // Message count or identifier
+    VectorClock vc;     // Vector clock associated with the message
+	
+    // Default constructor (required for serialization)
+    public Message(){}
+
+    // Constructor to initialize message from a string representation
+    public Message(String Msg)
+    {
+        // Split the message string using "|" as delimiter
+        String []SplitMsg = Msg.split("\\|");
+
+        // Parse the node ID and message count from the split parts
+        this.NodeId = Integer.parseInt(SplitMsg[0]);
+        this.MsgCount =  Integer.parseInt(SplitMsg[1]);
+
+        // Create a vector clock from the third part of the split message
+        this.vc = new VectorClock(NetworkSettings.TotalNode,SplitMsg[2]);
+        
+    }
+
+    // Constructor to initialize message with message count and vector clock
+    public Message(int MsgCount,VectorClock vc)
+    {
+        // Set the node ID to the current node's ID
+        this.NodeId = NetworkSettings.NodeID;
+        this.MsgCount = MsgCount;   // Set the message count
+        this.vc = vc;       // Set the vector clock
+    }
+
+    // Method to convert message object to string representation
+    public String ObjtoString()
+    {
+        // Convert the message fields to a string with "|" as delimiter
+        String MsgStr =  "" + this.NodeId +"|"+ this.MsgCount +"|"+ this.vc.toString().replaceAll(" ", "")
+                                                                         .replaceAll("\\[", "")
+                                                                         .replaceAll("]", "");
+        return MsgStr;      // Return the string representation of the message
+    }
+
+    // Method to print the message details
+    public void MsgPrint()
+    {
+        // Print node ID, message count, and vector clock
+        System.out.println("NodeId["+this.NodeId+"]  MsgNo["+this.MsgCount+"]   VectorClock["+this.vc.toString()+"]");
+    }
+}
